b1274d8655add3bf298775f8518a85750a569cab
unknown <afolabimuideen15@gmail.comgit config --global user.name Afolabi>
2022-08-09 06:31:27 +0100

    my javascript project

---
+++ script.js
@@ -0,0 +1,157 @@
+
+let displaymath = document.getElementById('display math')
+// let math = document.getElementById('math')
+function grade() {
+    if (math.value >= 70 && 100){
+        displaymath.innerHTML = "A Excellent";
+    }else
+    if(math.value >= 60 && 69) {
+        displaymath.innerHTML = "B very good";
+    }else
+    if(math.value >= 50 && 59) {
+        displaymath.innerHTML = "C good";
+    }else
+    if(math.value >= 40 && 49) { 
+        displaymath.innerHTML = "D pass";
+    }else
+    if(math.value <= 40) {
+        displaymath.innerHTML = "F fail"
+    }
+}
+
+// let displayeng = document.getElementById('display eng')
+// // let math = document.getElementById('math')
+// function grade() {
+//     if (eng.value >= 70 && 100){
+//         displayeng.innerHTML = "A Excellent";
+//     }else
+//     if(eng.value >= 60 && 69) {
+//         displayeng.innerHTML = "B very good";
+//     }else
+//     if(eng.value >= 50 && 59) {
+//         displayeng.innerHTML = "C good";
+//     }else
+//     if(eng.value >= 40 && 49) { 
+//         displayeng.innerHTML = "D pass";
+//     }else
+//     if(eng.value <= 40) {
+//         displayeng.innerHTML = "F fail"
+//     }
+// }
+// let displayphy = document.getElementById('display phy')
+// // let math = document.getElementById('math')
+// function grade() {
+//     if (phy.value >= 70 && 100){
+//         displayphy.innerHTML = "A Excellent";
+//     }else
+//     if(phy.value >= 60 && 69) {
+//         displayphy.innerHTML = "B very good";
+//     }else
+//     if(phy.value >= 50 && 59) {
+//         displayphy.innerHTML = "C good";
+//     }else
+//     if(phy.value >= 40 && 49) { 
+//         displayphy.innerHTML = "D pass";
+//     }else
+//     if(phy.value <= 40) {
+//         displayphy.innerHTML = "F fail"
+//     }
+// }
+//     let displaychem = document.getElementById('display chem')
+// // let math = document.getElementById('math')
+// function grade() {
+//     if (chem.value >= 70 && 100){
+//         displaychem.innerHTML = "A Excellent";
+//     }else
+//     if(chem.value >= 60 && 69) {
+//         displaychem.innerHTML = "B very good";
+//     }else
+//     if(chem.value >= 50 && 59) {
+//         displaychem.innerHTML = "C good";
+//     }else
+//     if(chem.value >= 40 && 49) { 
+//         displaychem.innerHTML = "D pass";
+//     }else
+//     if(chem.value <= 40) {
+//         displaychem.innerHTML = "F fail"
+//     }
+// }
+// let displaybio = document.getElementById('display bio')
+// // let math = document.getElementById('math')
+// function grade() {
+//     if (bio.value >= 70 && 100){
+//         displaybio.innerHTML = "A Excellent";
+//     }else
+//     if(bio.value >= 60 && 69) {
+//         displaybio.innerHTML = "B very good";
+//     }else
+//     if(bio.value >= 50 && 59) {
+//         displaybio.innerHTML = "C good";
+//     }else
+//     if(bio.value >= 40 && 49) { 
+//         displaybio.innerHTML = "D pass";
+//     }else
+//     if(bio.value <= 40) {
+//         displaybio.innerHTML = "F fail"
+//     }
+// }
+
+// let displaygeog = document.getElementById('display geog')
+// // let math = document.getElementById('math')
+// function grade() {
+//     if (geog.value >= 70 && 100){
+//         displaygeog.innerHTML = "A Excellent";
+//     }else
+//     if(geog.value >= 60 && 69) {
+//         displaygeog.innerHTML = "B very good";
+//     }else
+//     if(geog.value >= 50 && 59) {
+//         displaygeog.innerHTML = "C good";
+//     }else
+//     if(geog.value >= 40 && 49) { 
+//         displaygeog.innerHTML = "D pass";
+//     }else
+//     if(geog.value <= 40) {
+//         displaygeog.innerHTML = "F fail"
+//     }
+// }
+
+// let displayagric = document.getElementById('display agric')
+// // let math = document.getElementById('math')
+// function grade() {
+//     if (agric.value >= 70 && 100){
+//         displayagric.innerHTML = "A Excellent";
+//     }else
+//     if(agric.value >= 60 && 69) {
+//         displayagric.innerHTML = "B very good";
+//     }else
+//     if(agric.value >= 50 && 59) {
+//         displayagric.innerHTML = "C good";
+//     }else
+//     if(agric.value >= 40 && 49) { 
+//         displayagric.innerHTML = "D pass";
+//     }else
+//     if(agric.value <= 40) {
+//         displayagric.innerHTML = "F fail"
+//     }
+// }
+
+// let displayecons = document.getElementById('display econs')
+// // let math = document.getElementById('math')
+// function grade() {
+//     if (econs.value >= 70 && 100){
+//         displayecons.innerHTML = "A Excellent";
+//     }else
+//     if(econs.value >= 60 && 69) {
+//         displayecons.innerHTML = "B very good";
+//     }else
+//     if(econs.value >= 50 && 59) {
+//         displayecons.innerHTML = "C good";
+//     }else
+//     if(econs.value >= 40 && 49) { 
+//         displayecons.innerHTML = "D pass";
+//     }else
+//     if(econs.value <= 40) {
+//         displayecons.innerHTML = "F fail"
+//     }
+// }
